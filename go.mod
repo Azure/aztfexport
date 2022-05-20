fd9d54fddf1b2ec3fae0849adfc21534fbcd114f
magodo <wztdyl@sina.com>
2022-05-20 19:35:30 +0800

    Support Key Vault nested items
    The generated config for certificate can't pass validation, this needs
    to be fixed by the `tfadd`.
    
    Test needed.

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/Azure/aztfy
 go 1.18
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.23.1
-	github.com/Azure/azure-sdk-for-go/sdk/azidentity v0.14.0
-	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v0.5.0
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/keyvault/armkeyvault v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.0.0
 	github.com/charmbracelet/bubbles v0.10.4-0.20220412141214-292a1dd7ba97
 	github.com/charmbracelet/bubbletea v0.19.3
 	github.com/charmbracelet/lipgloss v0.4.0
@@ -23,7 +24,7 @@ require (
 )
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/internal v0.9.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v0.4.0 // indirect
 	github.com/agext/levenshtein v1.2.2 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
@@ -71,10 +72,10 @@ require (
 	github.com/vmihailenco/msgpack/v4 v4.3.12 // indirect
 	github.com/vmihailenco/tagparser v0.1.1 // indirect
 	github.com/zclconf/go-cty v1.10.0 // indirect
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
-	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
+	golang.org/x/crypto v0.0.0-20220511200225-c6db032c6c88 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- internal/armtemplate/armtemplate_hack.go
+++ internal/armtemplate/armtemplate_hack.go
@@ -1,6 +1,7 @@
 package armtemplate
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"strings"
@@ -9,20 +10,60 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-// PopulateManagedResources populate extra resources that are missing from ARM template due to they are exclusively managed by another resource.
-// E.g. managed disk resource id is not returned via ARM template as it is exclusively managed by VM.
-// Terraform models the resources differently than ARM template and needs to import those managed resources separately.
-func (tpl *Template) PopulateManagedResources() error {
+// TweakResources tweaks the resource set exported from ARM template, due to Terraform models the resources differently.
+func (tpl *Template) TweakResources() error {
+	// KeyVault certificate is a special resource that its data plane entity is composed of two control plane resources.
+	// ARM template exports the control plane resource ids, while Terraform uses its data plane counterpart.
+	if err := tpl.tweakForKeyVaultCertificate(); err != nil {
+		return err
+	}
+
+	// Populate exclusively managed resources that are missing from ARM template.
+	if err := tpl.populateManagedResources(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tpl *Template) tweakForKeyVaultCertificate() error {
+	newResoruces := []Resource{}
+	pending := map[string]Resource{}
+	for _, res := range tpl.Resources {
+		if res.Type != "Microsoft.KeyVault/vaults/keys" && res.Type != "Microsoft.KeyVault/vaults/secrets" {
+			newResoruces = append(newResoruces, res)
+			continue
+		}
+		if _, ok := pending[res.Name]; !ok {
+			pending[res.Name] = res
+			continue
+		}
+		delete(pending, res.Name)
+		newResoruces = append(newResoruces, Resource{
+			ResourceId: ResourceId{
+				Type: "Microsoft.KeyVault/vaults/certificates",
+				Name: res.Name,
+			},
+			Properties: nil,
+			DependsOn:  res.DependsOn,
+		})
+	}
+	for _, res := range pending {
+		newResoruces = append(newResoruces, res)
+	}
+	tpl.Resources = newResoruces
+	return nil
+}
+
+func (tpl *Template) populateManagedResources() error {
+	newResoruces := []Resource{}
 	knownManagedResourceTypes := map[string][]string{
 		"Microsoft.Compute/virtualMachines": {
 			"storageProfile.dataDisks.#.managedDisk.id",
 		},
 	}
-
-	newResoruces := []Resource{}
 	for _, res := range tpl.Resources {
 		if paths, ok := knownManagedResourceTypes[res.Type]; ok {
-			res, resources, err := populateManagedResources(res, paths...)
+			res, resources, err := populateManagedResourcesByPath(res, paths...)
 			if err != nil {
 				return fmt.Errorf(`populating managed resources for %q: %v`, res.Type, err)
 			}
@@ -36,9 +77,9 @@ func (tpl *Template) PopulateManagedResources() error {
 	return nil
 }
 
-// populateManagedResources populate the managed resources in the specified paths.
+// populateManagedResourcesByPath populate the managed resources in the specified paths.
 // It will also update the specified resource's dependency accordingly.
-func populateManagedResources(res Resource, paths ...string) (*Resource, []Resource, error) {
+func populateManagedResourcesByPath(res Resource, paths ...string) (*Resource, []Resource, error) {
 	b, err := json.Marshal(res.Properties)
 	if err != nil {
 		return nil, nil, fmt.Errorf("marshaling %v: %v", res.Properties, err)
@@ -86,11 +127,84 @@ func populateManagedResources(res Resource, paths ...string) (*Resource, []Resou
 // API interaction with ARM, where a non-nil client builder is required.
 func (res ResourceId) ProviderId(sub, rg string, b *client.ClientBuilder) (string, error) {
 	switch res.Type {
-	// See issue: https://github.com/Azure/aztfy/issues/89
 	case "microsoft.insights/webtests":
-		res.Type = "Microsoft.insights/webTests"
-		return res.ID(sub, rg), nil
+		return res.providerIdForInsightsWebtests(sub, rg, b)
+	case "Microsoft.KeyVault/vaults/keys",
+		"Microsoft.KeyVault/vaults/secrets",
+		"Microsoft.KeyVault/vaults/certificates":
+		return res.providerIdForKeyVaultNestedItems(sub, rg, b)
 	default:
 		return res.ID(sub, rg), nil
 	}
+}
+
+func (res ResourceId) providerIdForInsightsWebtests(sub, rg string, b *client.ClientBuilder) (string, error) {
+	// See issue: https://github.com/Azure/aztfy/issues/89
+	res.Type = "Microsoft.insights/webTests"
+	return res.ID(sub, rg), nil
+}
+
+func (res ResourceId) providerIdForKeyVaultNestedItems(sub, rg string, b *client.ClientBuilder) (string, error) {
+	// See issue: https://github.com/Azure/aztfy/issues/86
+	ctx := context.Background()
+	switch res.Type {
+	case "Microsoft.KeyVault/vaults/keys":
+		client, err := b.NewKeyvaultKeysClient(sub)
+		if err != nil {
+			return "", err
+		}
+		segs := strings.Split(res.Name, "/")
+		if len(segs) != 2 {
+			return "", fmt.Errorf("malformed resource name %q for %q", res.Type, res.Name)
+		}
+		resp, err := client.Get(ctx, rg, segs[0], segs[1], nil)
+		if err != nil {
+			return "", fmt.Errorf("retrieving %s: %v", res.ID(sub, rg), err)
+		}
+		if resp.Key.Properties == nil || resp.Key.Properties.KeyURIWithVersion == nil {
+			return "", fmt.Errorf("failed to get data plane URI from the response for %s", res.ID(sub, rg))
+		}
+		return *resp.Key.Properties.KeyURIWithVersion, nil
+	case "Microsoft.KeyVault/vaults/secrets":
+		client, err := b.NewKeyvaultSecretsClient(sub)
+		if err != nil {
+			return "", err
+		}
+		segs := strings.Split(res.Name, "/")
+		if len(segs) != 2 {
+			return "", fmt.Errorf("malformed resource name %q for %q", res.Type, res.Name)
+		}
+		resp, err := client.Get(ctx, rg, segs[0], segs[1], nil)
+		if err != nil {
+			return "", fmt.Errorf("retrieving %s: %v", res.ID(sub, rg), err)
+		}
+		if resp.Secret.Properties == nil || resp.Secret.Properties.SecretURIWithVersion == nil {
+			return "", fmt.Errorf("failed to get data plane URI from the response for %s", res.ID(sub, rg))
+		}
+		return *resp.Secret.Properties.SecretURIWithVersion, nil
+	case "Microsoft.KeyVault/vaults/certificates":
+		// There is no such type called "Microsoft.KeyVault/vaults/certificates" in ARM, this is just a hypothetic type to indicate
+		// the current resoruce corresponds to a certificate in data plane.
+		// We will use secret client to get its secret resource from control plane, and then construct the uri from the data plane uri of the secret.
+		client, err := b.NewKeyvaultSecretsClient(sub)
+		if err != nil {
+			return "", err
+		}
+		segs := strings.Split(res.Name, "/")
+		if len(segs) != 2 {
+			return "", fmt.Errorf("malformed resource name %q for %q", res.Type, res.Name)
+		}
+		resp, err := client.Get(ctx, rg, segs[0], segs[1], nil)
+		if err != nil {
+			return "", fmt.Errorf("retrieving %s: %v", res.ID(sub, rg), err)
+		}
+		if resp.Secret.Properties == nil || resp.Secret.Properties.SecretURIWithVersion == nil {
+			return "", fmt.Errorf("failed to get data plane URI from the response for %s (secret actually)", res.ID(sub, rg))
+		}
+		id := *resp.Secret.Properties.SecretURIWithVersion
+		segs = strings.Split(id, "/")
+		segs[len(segs)-3] = "certificates"
+		return strings.Join(segs, "/"), nil
+	}
+	panic("never reach here")
 }
