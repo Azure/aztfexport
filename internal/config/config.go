884c22dd6b69aa68035b85b1c37d464ae2b7fac8
magodo <wztdyl@sina.com>
2022-05-24 16:24:22 +0800

    Improve the CLI: adding long form option and env var source. (#111)

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,15 +3,15 @@ package config
 import "github.com/Azure/aztfy/internal/resmap"
 
 type Config struct {
-	SubscriptionId      string                 `env:"AZTFY_SUBSCRIPTION_ID" default:""`
-	ResourceGroupName   string                 // specified via CLI
-	Logfile             string                 `env:"AZTFY_LOGFILE" default:""`
-	MockClient          bool                   `env:"AZTFY_MOCK_CLIENT" default:"false"`
-	OutputDir           string                 // specified via CLI option
-	ResourceMapping     resmap.ResourceMapping // specified via CLI option
-	ResourceNamePattern string                 // specified via CLI option
-	Overwrite           bool                   // specified via CLI option
-	BatchMode           bool                   // specified via CLI option
-	BackendType         string                 // specified via CLI option
-	BackendConfig       []string               // specified via CLI option
+	LogPath             string
+	MockClient          bool
+	SubscriptionId      string
+	ResourceMapping     resmap.ResourceMapping
+	ResourceGroupName   string
+	OutputDir           string
+	ResourceNamePattern string
+	Overwrite           bool
+	BatchMode           bool
+	BackendType         string
+	BackendConfig       []string
 }
--- version.go
+++ version.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // To set this from outside, use go build -ldflags "-X 'main.version=$(VERSION)'"
 var version string = "dev"
 
@@ -7,3 +9,10 @@ var version string = "dev"
 // This value is extracted by git command when building.
 // To set this from outside, use go build -ldflags "-X 'main.revision=$(REVISION)'"
 var revision string
+
+func getVersion() string {
+	if revision != "" {
+		return fmt.Sprintf("%s(%s)\n", version, revision)
+	}
+	return version
+}
