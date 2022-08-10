8a743361f4f39dedffb7266a0c0d44dc553124c2
magodo <wztdyl@sina.com>
2022-08-10 11:24:08 +0800

    Parallelize the build via pipeline job strategy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,15 @@
 #!/usr/bin/env bash
+
+# Usage: build.sh OS ARCH VERSION
+# E.g. build.sh linux amd64 v0.1.0
 REVISION=$(git rev-parse --short HEAD)
-OS_ARCH=("freebsd:amd64"
-  "freebsd:386"
-  "freebsd:arm"
-  "freebsd:arm64"
-  "windows:amd64"
-  "windows:386"
-  "linux:amd64"
-  "linux:386"
-  "linux:arm"
-  "linux:arm64"
-  "darwin:amd64"
-  "darwin:arm64")
 mkdir dist
-for os_arch in "${OS_ARCH[@]}" ; do
-  OS=${os_arch%%:*}
-  ARCH=${os_arch#*:}
-  echo "GOOS: ${OS}, GOARCH: ${ARCH}"
-
-  output=aztfy_${OS}_${ARCH}
-  if [[ $OS = windows ]]; then
+OS=$1
+ARCH=$2
+VERSION=$3
+echo "OS: ${OS}, ARCH: ${ARCH}, VERSION: ${VERSION}"
+output=aztfy_${OS}_${ARCH}
+if [[ $OS = windows ]]; then
     output=$output.exe
-  fi
-
-  GOOS="${OS}" GOARCH="${ARCH}" CGO_ENABLED=0 go build -trimpath -ldflags="-s -w -X 'main.version=${VERSION}' -X 'main.revision=${REVISION}'" -o $output
-  mv $output dist
-done
+fi
+GOOS="${OS}" GOARCH="${ARCH}" CGO_ENABLED=0 go build -trimpath -ldflags="-s -w -X 'main.version=${VERSION}' -X 'main.revision=${REVISION}'" -o dist/$output
