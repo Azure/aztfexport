9296eadedc7227ee1d30754f7af14a4072f6b923
magodo <wztdyl@sina.com>
2022-08-19 17:32:27 +0800

    verify the correct version is installed

--- scripts/package/linux/verify/ubuntu-bionic.sh
+++ scripts/package/linux/verify/ubuntu-bionic.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+version=${1:?"version not specified"}
+
 apt-get update
 apt-get install -y curl software-properties-common gpg
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 apt-add-repository https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-multiarch-prod
 apt-get update
 apt-get install -y aztfy
-aztfy -v
+grep $version <(aztfy -v)
