1a0b815c2d74dabe9a52d8d606a8b0f68ac1e62e
magodo <wztdyl@sina.com>
2022-08-19 18:14:46 +0800

    verify debian releases

---
+++ scripts/package/linux/verify/debian-buster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+version=${1:?"version not specified"}
+
+apt-get update
+apt-get install -y curl software-properties-common gpg
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+apt-add-repository https://packages.microsoft.com/debian/10/prod
+apt-get update
+apt-get install -y aztfy
+grep $version <(aztfy -v)
