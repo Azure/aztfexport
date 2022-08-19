532755693f0bc283182aabaf2a82ed64253e4c7e
magodo <wztdyl@sina.com>
2022-08-19 17:44:27 +0800

    verify all ubuntu releases

--- scripts/package/linux/verify/ubuntu-focal.sh
+++ scripts/package/linux/verify/ubuntu-focal.sh
@@ -5,8 +5,8 @@ version=${1:?"version not specified"}
 
 apt-get update
 apt-get install -y curl software-properties-common gpg
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc > /etc/apt/trusted.gpg.d/microsoft.asc
+apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
 apt-get update
 apt-get install -y aztfy
 grep $version <(aztfy -v)
