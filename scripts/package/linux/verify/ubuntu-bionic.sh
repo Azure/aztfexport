bf1fc46fdaf8729f77acf879b26b292174c66be3
magodo <wztdyl@sina.com>
2022-08-19 17:51:55 +0800

    fix

--- scripts/package/linux/verify/ubuntu-bionic.sh
+++ scripts/package/linux/verify/ubuntu-bionic.sh
@@ -6,7 +6,7 @@ version=${1:?"version not specified"}
 apt-get update
 apt-get install -y curl software-properties-common gpg
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch
+apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch/prod
 apt-get update
 apt-get install -y aztfy
 grep $version <(aztfy -v)
--- scripts/package/linux/verify/ubuntu-jammy.sh
+++ scripts/package/linux/verify/ubuntu-jammy.sh
@@ -6,7 +6,7 @@ version=${1:?"version not specified"}
 apt-get update
 apt-get install -y curl software-properties-common gpg
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc > /etc/apt/trusted.gpg.d/microsoft.asc
-apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
+apt-add-repository https://packages.microsoft.com/ubuntu/22.04/prod
 apt-get update
 apt-get install -y aztfy
 grep $version <(aztfy -v)
