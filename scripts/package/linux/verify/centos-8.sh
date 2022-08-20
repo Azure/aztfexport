159d406e0e37074ae911a6afac0cf63a13ba72d1
magodo <wztdyl@sina.com>
2022-08-20 11:14:57 +0800

    verify all

---
+++ scripts/package/linux/verify/centos-8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+version=${1:?"version not specified"}
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+dnf install -y https://packages.microsoft.com/config/centos/8/packages-microsoft-prod.rpm
+dnf check-update
+dnf install -y aztfy
+grep $version <(aztfy -v)
---
+++ scripts/package/linux/verify/fedora-34.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+version=${1:?"version not specified"}
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+dnf install -y https://packages.microsoft.com/config/fedora/34/packages-microsoft-prod.rpm
+dnf check-update
+dnf install -y aztfy
+grep $version <(aztfy -v)
