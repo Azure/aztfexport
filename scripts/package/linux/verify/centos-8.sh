491125e92831af26f03c2b6c3108d4dd28d6bcfa
magodo <wztdyl@sina.com>
2022-08-20 11:28:07 +0800

    handle dnf check-update returns 100

--- scripts/package/linux/verify/centos-8.sh
+++ scripts/package/linux/verify/centos-8.sh
@@ -7,6 +7,9 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 dnf install -y https://packages.microsoft.com/config/centos/8/packages-microsoft-prod.rpm
-dnf check-update
+
+# See: https://access.redhat.com/solutions/2779441
+dnf check-update || [[ $? == 100 ]]  
+
 dnf install -y aztfy
 grep $version <(aztfy -v)
--- scripts/package/linux/verify/fedora-34.sh
+++ scripts/package/linux/verify/fedora-34.sh
@@ -5,6 +5,9 @@ version=${1:?"version not specified"}
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 dnf install -y https://packages.microsoft.com/config/fedora/34/packages-microsoft-prod.rpm
-dnf check-update
+
+# See: https://access.redhat.com/solutions/2779441
+dnf check-update || [[ $? == 100 ]]  
+
 dnf install -y aztfy
 grep $version <(aztfy -v)
