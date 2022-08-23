e21a96eeb566699d7e3a614c17eb6cc2a32f6586
magodo <wztdyl@sina.com>
2022-08-23 11:16:50 +0800

    fedora repos remove repo configs for accelarating

--- scripts/package/linux/verify/fedora-35.sh
+++ scripts/package/linux/verify/fedora-35.sh
@@ -4,6 +4,10 @@ set -e
 version=${1:?"version not specified"}
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# To accelarate syncing the repo metadatas
+rm -f /etc/yum.repos.d/*
+
 dnf install -y https://packages.microsoft.com/config/fedora/35/packages-microsoft-prod.rpm
 
 # See: https://access.redhat.com/solutions/2779441
