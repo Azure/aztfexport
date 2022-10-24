996777aae4c76893ab90eef0205e56e1b081dd48
magodo <wztdyl@sina.com>
2022-10-24 11:33:17 +0800

    Integrate with `gosec` to CI (#270)

--- internal/utils/writefile.go
+++ internal/utils/writefile.go
@@ -3,17 +3,18 @@ package utils
 import "os"
 
 func WriteFileSync(name string, data []byte, perm os.FileMode) error {
+	// #nosec G304
 	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
 	if err != nil {
 		return err
 	}
+	// #nosec G307
+	defer f.Close()
 	if _, err = f.Write(data); err != nil {
-		f.Close()
 		return err
 	}
 	if err := f.Sync(); err != nil {
-		f.Close()
 		return err
 	}
-	return f.Close()
+	return nil
 }
