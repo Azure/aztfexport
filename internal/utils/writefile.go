148fe0c165b6bc9d11f6e638fdccb6f0b74e8c4f
magodo <wztdyl@sina.com>
2022-10-24 11:05:43 +0800

    Revert "Try to include issue"
    This reverts commit 76aaec3c0206daefa8d80a6e561c78ba10a49f0c.

--- internal/utils/writefile.go
+++ internal/utils/writefile.go
@@ -8,6 +8,7 @@ func WriteFileSync(name string, data []byte, perm os.FileMode) error {
 	if err != nil {
 		return err
 	}
+	// #nosec G307
 	defer f.Close()
 	if _, err = f.Write(data); err != nil {
 		return err
