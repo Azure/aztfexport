14d962b1d60ad357239de03ce878abea1c4032c7
Gerry Tan <gerry.tan@microsoft.com>
2025-05-22 14:37:06 +1000

    Replace transitive dependencies with TF address

--- internal/meta/hcl_edit_test.go
+++ internal/meta/hcl_edit_test.go
@@ -198,3 +198,156 @@ func hclwriteBody(t *testing.T, input string) *hclwrite.Body {
 	}
 	return file.Body()
 }
+
+func TestHclBlockUpdateDependency(t *testing.T) {
+	cases := []struct {
+		description     string
+		deps            []Dependency
+		cfgset          map[string]ConfigInfo
+		inputHclBody    string
+		expectedHclBody string
+	}{
+		{
+			description: "foo_id should be replaced with TF address, no depends_on is added",
+			deps: []Dependency{
+				{[]string{"/subscriptions/123/resourceGroups/123"}},
+				{[]string{"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123"}},
+			},
+			cfgset: map[string]ConfigInfo{
+				"/subscriptions/123/resourceGroups/123": configInfo(
+					t,
+					"/subscriptions/123/resourceGroups/123",
+					"azurerm_resource_group.res-0",
+				),
+				"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123": configInfo(
+					t,
+					"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123",
+					"azurerm_foo_resource.res-1",
+				),
+			},
+			inputHclBody: `
+name                = "test"
+resource_group_name = "test"
+foo_id              = "/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123"
+`,
+			expectedHclBody: `
+name                = "test"
+resource_group_name = "test"
+foo_id              = azurerm_foo_resource.res-1.id
+`,
+		},
+		{
+			description: `depends on foo by foo_name, 
+			depends on resource group by resource_group_id, 
+			foo also depends on the same resource group: 
+			expected resource_group_id to be replaced with TF address and there is a depends_on on foo`,
+			deps: []Dependency{
+				{[]string{"/subscriptions/123/resourceGroups/123"}},
+				{[]string{"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123"}},
+			},
+			cfgset: map[string]ConfigInfo{
+				"/subscriptions/123/resourceGroups/123": configInfo(
+					t,
+					"/subscriptions/123/resourceGroups/123",
+					"azurerm_resource_group.res-0",
+				),
+				"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123": configInfo(
+					t,
+					"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123",
+					"azurerm_foo_resource.res-1",
+				),
+			},
+			inputHclBody: `
+name              = "test"
+resource_group_id = "/subscriptions/123/resourceGroups/123"
+foo_name          = "test"
+`,
+			expectedHclBody: `
+name              = "test"
+resource_group_id = azurerm_resource_group.res-0.id
+foo_name          = "test"
+depends_on = [
+  azurerm_foo_resource.res-1
+]
+`,
+		},
+		{
+			description: "ambiguous dependency explicitly specified via depends_on",
+			deps: []Dependency{
+				{[]string{"/subscriptions/123/resourceGroups/123"}},
+				{[]string{
+					"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123/subfoo/1",
+					"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123/subfoo/2",
+				}},
+			},
+			cfgset: map[string]ConfigInfo{
+				"/subscriptions/123/resourceGroups/123": configInfo(
+					t,
+					"/subscriptions/123/resourceGroups/123",
+					"azurerm_resource_group.res-0",
+				),
+				"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123/subfoo/1": configInfo(
+					t,
+					"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123",
+					"azurerm_subfoo_resource.res-1",
+				),
+				"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123/subfoo/2": configInfo(
+					t,
+					"/subscriptions/123/resourceGroups/123/providers/Microsoft.Foo/foo/123",
+					"azurerm_subfoo_resource.res-2",
+				),
+			},
+			inputHclBody: `
+name                = "test"
+resource_group_name = "test"
+`,
+			expectedHclBody: `
+name                = "test"
+resource_group_name = "test"
+depends_on = [
+  # One of azurerm_subfoo_resource.res-1,azurerm_subfoo_resource.res-2 (can't auto-resolve as their ids are identical)
+  azurerm_resource_group.res-0
+]
+`,
+		},
+	}
+
+	for _, c := range cases {
+		body := hclwriteBody(t, c.inputHclBody)
+		require.NoError(t, hclBlockUpdateDependency(body, c.deps, c.cfgset), c.description)
+		require.Equal(t, string(hclwrite.Format(body.BuildTokens(nil).Bytes())), c.expectedHclBody, c.description)
+	}
+}
+
+func TestDescendantResourceIdInCollection(t *testing.T) {
+	cases := []struct {
+		description     string
+		resourceId      string
+		resourceIdsList []string
+		expectedRetVal  bool
+	}{
+		{
+			description: "descendant resource id not in collection",
+			resourceId:  "/subscriptions/123/resourceGroups/123",
+			resourceIdsList: []string{
+				"/subscriptions/123/resourceGroups/456/providers/Microsoft.Bar/bar/123",
+				"/subscriptions/123/resourceGroups/456/providers/Microsoft.Bar/bar/123/subbar/456",
+			},
+			expectedRetVal: false,
+		},
+		{
+			description: "descendant resource id in collection",
+			resourceId:  "/subscriptions/123/resourceGroups/123",
+			resourceIdsList: []string{
+				"/subscriptions/123/resourceGroups/456/providers/Microsoft.Foo/foo/aaa",
+				"/subscriptions/123/resourceGroups/123/providers/Microsoft.Bar/bar/bbb",
+			},
+			expectedRetVal: true,
+		},
+	}
+
+	for _, c := range cases {
+		actualRetVal := descendantResourceIdInCollection(c.resourceId, &c.resourceIdsList)
+		require.Equal(t, c.expectedRetVal, actualRetVal, "'%s': expectedRetVal should match actual", c.description)
+	}
+}
